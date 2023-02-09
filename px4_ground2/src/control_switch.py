69b297b3476a954eb0335711d8fe9fc36cb576a0
TPODAvia <thepowerofdarknes2000@gmail.com>
2023-02-09 10:54:52 +0300

    px4_ground change

---
+++ px4_ground2/src/control_switch.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import PoseStamped, TwistStamped, Twist
+from mavros_msgs.msg import State
+from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest
+
+current_state = State()
+
+g_vel = Twist()
+
+def state_cb(msg):
+    global current_state
+    current_state = msg
+
+def state_callback(data):
+    global g_vel
+    # rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.angular.x)
+    g_vel = data
+
+if __name__ == "__main__":
+    rospy.init_node("offb_node_py")
+
+    state_sub = rospy.Subscriber("mavros/state", State, callback = state_cb)
+    
+    rospy.wait_for_service("/mavros/cmd/arming")
+    arming_client = rospy.ServiceProxy("mavros/cmd/arming", CommandBool)    
+
+    rospy.wait_for_service("/mavros/set_mode")
+    set_mode_client = rospy.ServiceProxy("mavros/set_mode", SetMode)
+    
+
+    # Setpoint publishing MUST be faster than 2Hz
+    rate = rospy.Rate(20)
+
+    # Wait for Flight Controller connection
+    while(not rospy.is_shutdown() and not current_state.connected):
+        rate.sleep()
+
+    offb_set_mode = SetModeRequest()
+    offb_set_mode.custom_mode = 'OFFBOARD'
+
+    arm_cmd = CommandBoolRequest()
+    arm_cmd.value = True
+
+    last_req = rospy.Time.now()
+
+    while(not rospy.is_shutdown()):
+        rospy.Subscriber('cmd_vel', Twist, callback = state_callback)
+
+        if(current_state.mode != "OFFBOARD" and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
+            if(set_mode_client.call(offb_set_mode).mode_sent == True):
+                rospy.loginfo("OFFBOARD enabled")
+            
+            last_req = rospy.Time.now()
+        else:
+            if(not current_state.armed and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
+                if(arming_client.call(arm_cmd).success == True):
+                    rospy.loginfo("Vehicle armed")
+            
+                last_req = rospy.Time.now()
+
+        rate.sleep()
