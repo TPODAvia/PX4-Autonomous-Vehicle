cf9d3c38a8b6f0636018d054d5ed6958490d258e
TPODAvia <thepowerofdarknes2000@gmail.com>
2023-02-24 12:13:48 +0300

    added cpp and python files Changes to be committed: 	new file:   clover/launch/mavros_config2.yaml 	modified:   px4_ground/CMakeLists.txt 	new file:   px4_ground/airframes_sitl/1061_diff_rover2 	new file:   px4_ground/include/auto_2d_nav.h 	new file:   px4_ground/include/landing_quadrotor.h 	new file:   px4_ground/include/mission_car_offboard.h 	new file:   px4_ground/include/offboard_control.h 	new file:   px4_ground/include/px4_control_cfg.h 	new file:   px4_ground/include/ros_nav_quadrotor.h 	new file:   px4_ground/include/tracking_quadrotor.h 	modified:   px4_ground/package.xml 	renamed:    px4_ground/src/control_pos.py -> px4_ground/script/control_pos.py 	renamed:    px4_ground/src/control_vel.py -> px4_ground/script/control_vel.py 	new file:   px4_ground/script/keyboard_control_px4.py 	new file:   px4_ground/script/mavros_mission_px4.py 	renamed:    px4_ground/src/mavros_offboard_attctl_test.py -> px4_ground/script/mavros_offboard_attctl_test.py 	renamed:    px4_ground/src/mavros_offboard_posctl_test.py -> px4_ground/script/mavros_offboard_posctl_test.py 	renamed:    px4_ground/src/mavros_test_common.py -> px4_ground/script/mavros_test_common.py 	new file:   px4_ground/script/rover_keyboard_control_px4.py 	renamed:    px4_ground/src/teleop_twist_keyboard.py -> px4_ground/script/teleop_twist_keyboard.py 	renamed:    px4_ground/src/wind.py -> px4_ground/script/wind.py 	deleted:    px4_ground/src/__pycache__/mavros_test_common.cpython-38.pyc 	new file:   px4_ground/src/auto_2d_nav.cpp 	new file:   px4_ground/src/avoidance_car_vfh.cpp 	new file:   px4_ground/src/circular.cpp 	new file:   px4_ground/src/circular_car.cpp 	deleted:    px4_ground/src/landing_detect.py 	deleted:    px4_ground/src/mavros_offboard_yawrate_test.py 	deleted:    px4_ground/src/mission_test.py

---
+++ px4_ground/include/px4_control_cfg.h
@@ -0,0 +1,124 @@
+/*
+ * px4_control_cfg.h
+ *
+ *  Created on: 2018-5-4
+ *      Author: zipout
+ *///Email		  : 1554459957@qq.com
+
+#ifndef PX4_CONTROL_CFG_H_
+#define PX4_CONTROL_CFG_H_
+#include <ros/ros.h>
+#include <iostream>
+using namespace std;
+ typedef struct
+	{
+		float vel_x;
+		float vel_y;
+		float vel_z;
+	} S_SETPOINT_VEL;
+	typedef struct
+	{
+		float difference;      //比例项
+		float differential;    //微分项
+		float tempDiffer;			 //上一时刻的比例项
+		float intergral;       //积分项
+	} S_PID_ITEM;
+	typedef struct
+	{
+		float p;       //比例项系数
+		float d;       //微分项系数.
+		float i;       //积分项系数
+	} S_PID;
+#endif /* PX4_CONTROL_CFG_H_ */
+#include <ros/ros.h>
+#include <iostream>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+#include <geometry_msgs/Twist.h>
+#include <mavros_msgs/PositionTarget.h>
+#include <Eigen/Dense>
+#include "offboard_control.h"
+using namespace std;
+using namespace Eigen;
+class PX4RosNav {
+ public:
+    /**
+     *默认构造函数
+     */
+    PX4RosNav(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private);
+    /**
+     * 析构函数
+     */
+    ~PX4RosNav();
+    void initialize();
+
+  /**
+  * @brief      check healthiness of the avoidance system to trigger failsafe in
+  *             the FCU
+  * @param[in]  since_last_cloud, time elapsed since the last waypoint was
+  *             published to the FCU
+  * @param[in]  since_start, time elapsed since staring the node
+  * @param[out] planner_is_healthy, true if the planner is running without
+  *errors
+  * @param[out] hover, true if the vehicle is hovering
+  **/
+  OffboardControl OffboardControl_;
+ private:
+  ros::NodeHandle nh_;
+  ros::NodeHandle nh_private_;
+  ros::Timer cmdloop_timer_;
+  void CmdLoopCallback(const ros::TimerEvent& event);
+  void PublishVelControl();
+  void CmdVelCallback(const geometry_msgs::Twist &msg);
+  Eigen::Vector3d  px4_vel_;
+
+  float desire_posz_;
+  ros::Subscriber cmd_vel_sub_;
+};
+#include <ros/ros.h>
+#include <iostream>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+#include <geometry_msgs/Twist.h>
+#include <mavros_msgs/PositionTarget.h>
+#include <Eigen/Dense>
+#include "offboard_control.h"
+using namespace std;
+using namespace Eigen;
+class PX4RosNav {
+ public:
+    /**
+     *默认构造函数
+     */
+    PX4RosNav(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private);
+    /**
+     * 析构函数
+     */
+    ~PX4RosNav();
+    void initialize();
+
+  /**
+  * @brief      check healthiness of the avoidance system to trigger failsafe in
+  *             the FCU
+  * @param[in]  since_last_cloud, time elapsed since the last waypoint was
+  *             published to the FCU
+  * @param[in]  since_start, time elapsed since staring the node
+  * @param[out] planner_is_healthy, true if the planner is running without
+  *errors
+  * @param[out] hover, true if the vehicle is hovering
+  **/
+  OffboardControl OffboardControl_;
+ private:
+  ros::NodeHandle nh_;
+  ros::NodeHandle nh_private_;
+  ros::Timer cmdloop_timer_;
+  void CmdLoopCallback(const ros::TimerEvent& event);
+  void PublishVelControl();
+  void CmdVelCallback(const geometry_msgs::Twist &msg);
+  Eigen::Vector3d  px4_vel_;
+
+  float desire_posz_;
+  ros::Subscriber cmd_vel_sub_;
+};
